98c2757e4e89ce3cfe803bdf5f5958789640c955
tabsheermk <creativemk12@gmail.com>
2025-01-09 21:03:07 +0530

    events put, get, delete created

---
+++ app/api/events/[id]/route.ts
@@ -0,0 +1,39 @@
+import { Event, events } from "@/lib/data";
+import { NextResponse } from "next/server";
+
+// GET /api/events/[id] => Get details for a specific event
+export async function GET(request: Request, {params}: {params: {id: string}}) {
+    const id = parseInt(params.id);
+    const event = events.find((event) => event.id === id);
+    if (event) {
+        return NextResponse.json(event);
+    } else {
+        return NextResponse.json({message: "Event Not Found"},{status: 404});
+    }
+}
+
+// PUT /api/events/[id] => Update the details of a specific event
+export async function PUT(request: Request, {params}: {params: {id: string}}) {
+    const id = parseInt(params.id);
+    const updatedEvent: Event = await request.json();
+    const eventIndex = events.findIndex((event) => event.id === id)
+
+    if(eventIndex !== -1) {
+        events[eventIndex] = {...events[eventIndex], ...updatedEvent};
+        return NextResponse.json(events[eventIndex]);
+    } else {
+        return NextResponse.json({message: "Event Not Found"}, {status: 404});
+    }
+}
+
+// DELETE /api/events/[id] => Delete an event
+export async function DELETE(request: Request, {params}: {params: {id: string}}) {
+    const id = parseInt(params.id);
+    const eventIndex = events.findIndex((event) => event.id === id);
+    if (eventIndex !== -1) {
+        events.splice(eventIndex, 1);
+        return NextResponse.json({message: "Event deleted successffuly"});
+    } else {
+        return NextResponse.json({message: "Event Not Found"}, {status: 404});
+    }
+}
